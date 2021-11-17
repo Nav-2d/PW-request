6b2c103573447936c805ef4f6a5fa802c6a7331f
nav-2d <navdeep.singh2088@gmail.com>
2021-11-17 11:31:27 -0800

    Update package.json

--- foo.spec.js
+++ foo.spec.js
@@ -3,11 +3,8 @@ const email = 'MY_EMAIL'
 const password = 'MY_PASSWORD'
 test('should create a bug report', async ({ request }) => {
   const response = await request.post(`MY_URL`, {
-    data: {
-      email,
-      password,
-    },
     headers: { 'accept': '*/*' },
+    data: {email,password,},
   })
   expect(response.status()).toBe(403)
 });
